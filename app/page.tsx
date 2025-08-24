d4f85eaedd66df3d52d83a6ba84c12be4a295208
lelekara <lelekara04147@gmail.com>
2025-08-24 12:58:00 +0200

    Implement code changes to enhance functionality and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,12 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+              <img
+                src="/img/logo-pino.png"
+                alt="Logo"
+                className="h-8 w-8 rounded-sm"
+              />
+              <Link href={"/"}>Les Scout Pionnier</Link>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
