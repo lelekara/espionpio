691a22380676e8dca418625aca0163fb52f12183
lelekara <lelekara04147@gmail.com>
2025-09-22 20:02:39 +0200

    fix: update footer link to point to personal portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <p>
             Powered by{" "}
             <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              href="https://lelekara.github.io/portfolio"
               target="_blank"
               className="font-bold hover:underline"
               rel="noreferrer"
